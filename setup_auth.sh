f8b66ec029c642b20333a22ba76d0abbcfc95a79
Way Barrios <waybarrios@gmail.com>
2017-01-18 14:21:33 -0500

    Overwriting config xml files on geoserver container

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-sed -i.bak 's@<baseUrl>\([^<][^<]*\)</baseUrl>@<baseUrl>'"$DJANGO_URL"'</baseUrl>@'\
-           /geoserver_data/data/security/auth/geonodeAuthProvider/config.xml
+sed -i 's@http://localhost:8000/@'"$SITEURL"'@g' /geoserver_data/data/security/filter/geonode-oauth2/config.xml
+sed -i 's@http://localhost:8080/geoserver@'"$GEOSERVER_PUBLIC_LOCATION"'@g' /geoserver_data/data/security/filter/geonode-oauth2/config.xml
+sed -i.bak 's@<baseUrl>\([^<][^<]*\)</baseUrl>@<baseUrl>'"$SITEURL"'</baseUrl>@'\
+           /geoserver_data/data/security/role/geonode\ REST\ role\ service/config.xml
