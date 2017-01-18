5d4a9919b04927efa5c740c422913e9390b37066
Way Barrios <waybarrios@gmail.com>
2017-01-18 14:52:26 -0500

    Deleting not working authentication

--- setup_auth.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sed -i 's@http://localhost:8000/@'"$SITEURL"'@g' /geoserver_data/data/security/filter/geonode-oauth2/config.xml
-sed -i 's@http://localhost:8080/geoserver@'"$GEOSERVER_PUBLIC_LOCATION"'@g' /geoserver_data/data/security/filter/geonode-oauth2/config.xml
-sed -i.bak 's@<baseUrl>\([^<][^<]*\)</baseUrl>@<baseUrl>'"$SITEURL"'</baseUrl>@'\
-           /geoserver_data/data/security/role/geonode\ REST\ role\ service/config.xml
