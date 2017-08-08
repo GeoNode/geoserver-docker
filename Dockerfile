ffc8400fff10c4f30614fa899656570915e5f6b9
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-08-08 12:26:03 +0200

    Add control for docker4mac/windows

--- Dockerfile
+++ Dockerfile
@@ -11,48 +11,75 @@ ENV GEOSERVER_DATA_DIR="/geoserver_data/data"
 # Download and install GeoServer
 #
 RUN cd /usr/local/tomcat/webapps \
-    && wget --progress=bar:force:noscroll http://build.geonode.org/geoserver/latest/geoserver-${GEOSERVER_VERSION}.war \
+    && wget --progress=bar:force:noscroll \
+    http://build.geonode.org/geoserver/latest/geoserver-${GEOSERVER_VERSION}.war \
     && unzip -q geoserver-${GEOSERVER_VERSION}.war -d geoserver \
     && rm geoserver-${GEOSERVER_VERSION}.war \
     && mkdir -p $GEOSERVER_DATA_DIR
 
 VOLUME $GEOSERVER_DATA_DIR
 
-# Set DOCKER_HOST address
-ARG DOCKER_HOST=${DOCKER_HOST}
+###########docker host###############
+# Set DOCKERHOST variable if DOCKER_HOST exists
+ARG DOCKERHOST=${DOCKERHOST}
 # for debugging
-RUN echo -n DOCKER_HOST=$DOCKER_HOST
+RUN echo -n #1===>DOCKERHOST=${DOCKERHOST}
 #
-ENV DOCKER_HOST ${DOCKER_HOST}
+ENV DOCKERHOST ${DOCKERHOST}
 # for debugging
-RUN echo -n DOCKER_HOST=$DOCKER_HOST
+RUN echo -n #2===>DOCKERHOST=${DOCKERHOST}
 
-ENV DOCKER_HOST_IP ${DOCKER_HOST_IP}
-RUN echo export DOCKER_HOST_IP=${DOCKER_HOST} | sed 's/tcp:\/\/\([^:]*\).*/\1/' >> /root/.bashrc
-
+###########docker host ip#############
+# Set GEONODE_HOST_IP address if it exists
+ARG GEONODE_HOST_IP=${GEONODE_HOST_IP}
 # for debugging
-RUN echo -n DOCKER_HOST_IP=${DOCKER_HOST_IP}
-# ENV DOCKER_HOST_IP ${DOCKER_HOST_IP}
+RUN echo -n #1===>GEONODE_HOST_IP=${GEONODE_HOST_IP}
+#
+ENV GEONODE_HOST_IP ${GEONODE_HOST_IP}
+# for debugging
+RUN echo -n #2===>GEONODE_HOST_IP=${GEONODE_HOST_IP}
+# If empty set DOCKER_HOST_IP to GEONODE_HOST_IP
+ENV DOCKER_HOST_IP=${DOCKER_HOST_IP:-${GEONODE_HOST_IP}}
+# for debugging
+RUN echo -n #1===>DOCKER_HOST_IP=${DOCKER_HOST_IP}
+# Trying to set the value of DOCKER_HOST_IP from DOCKER_HOST
+RUN if ! [ -z ${DOCKER_HOST_IP} ]; \
+    then echo export DOCKER_HOST_IP=${DOCKERHOST} | \
+    sed 's/tcp:\/\/\([^:]*\).*/\1/' >> /root/.bashrc; \
+    else echo "DOCKER_HOST_IP is already set!"; fi
+# for debugging
+RUN echo -n #2===>DOCKER_HOST_IP=${DOCKER_HOST_IP}
+
 # Set WEBSERVER public port
 ARG PUBLIC_PORT=${PUBLIC_PORT}
 # for debugging
-RUN echo -n PUBLIC_PORT=${PUBLIC_PORT}
+RUN echo -n #1===>PUBLIC_PORT=${PUBLIC_PORT}
 #
 ENV PUBLIC_PORT=${PUBLIC_PORT}
 # for debugging
-RUN echo -n PUBLIC_PORT=${PUBLIC_PORT}
+RUN echo -n #2===>PUBLIC_PORT=${PUBLIC_PORT}
 
 # set nginx base url for geoserver
-RUN echo export BASE_URL=http://${NGINX_HOST}:${NGINX_PORT}/ | sed 's/tcp:\/\/\([^:]*\).*/\1/' >> /root/.bashrc
+RUN echo export BASE_URL=http://${NGINX_HOST}:${NGINX_PORT}/ | \
+    sed 's/tcp:\/\/\([^:]*\).*/\1/' >> /root/.bashrc
 
-# copy the script and perform the change to config.xml
+# copy the script and perform the run of scripts from entrypoint.sh
 RUN mkdir -p /usr/local/tomcat/tmp
 WORKDIR /usr/local/tomcat/tmp
 COPY set_geoserver_auth.sh /usr/local/tomcat/tmp
-RUN chmod +x /usr/local/tomcat/tmp/set_geoserver_auth.sh
-
 COPY setup_auth.sh /usr/local/tomcat/tmp
-RUN chmod +x /usr/local/tomcat/tmp/setup_auth.sh
-COPY entrypoint.sh /usr/local/tomcat/tmp/entrypoint.sh
-RUN chmod +x /usr/local/tomcat/tmp/entrypoint.sh
+COPY requirements.txt /usr/local/tomcat/tmp
+COPY get_dockerhost_ip.py /usr/local/tomcat/tmp
+COPY entrypoint.sh /usr/local/tomcat/tmp
+
+RUN apt-get update \
+    && apt-get -y upgrade \
+    && apt-get install -y python python-pip python-dev \
+    && chmod +x /usr/local/tomcat/tmp/set_geoserver_auth.sh \
+    && chmod +x /usr/local/tomcat/tmp/setup_auth.sh \
+    && chmod +x /usr/local/tomcat/tmp/entrypoint.sh \
+    && pip install --upgrade pip \
+    && pip install -r requirements.txt \
+    && chmod +x /usr/local/tomcat/tmp/get_dockerhost_ip.py
+
 CMD ["/usr/local/tomcat/tmp/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,42 +4,45 @@ set -e
 source /root/.bashrc
 
 # control the value of DOCKER_HOST_IP variable
-if ! [ -z "$DOCKER_HOST_IP" ]
+if ! [ -z ${DOCKER_HOST_IP} ]
 then
 
-    # set basic tagname
-    TAGNAME=( "baseUrl" )
-
-    # backup geonodeAuthProvider config.xml
-    cp ${GEOSERVER_DATA_DIR}/security/auth/geonodeAuthProvider/config.xml ${GEOSERVER_DATA_DIR}/security/auth/geonodeAuthProvider/config.xml.orig
-    # run the setting script for geonodeAuthProvider
-    /usr/local/tomcat/tmp/set_geoserver_auth.sh ${GEOSERVER_DATA_DIR}/security/auth/geonodeAuthProvider/config.xml ${GEOSERVER_DATA_DIR}/security/auth/geonodeAuthProvider/ ${TAGNAME} >> /usr/local/tomcat/tmp/set_geoserver_auth.log
-
-    # backup geonode REST role service config.xml
-    cp "${GEOSERVER_DATA_DIR}/security/role/geonode REST role service/config.xml" "${GEOSERVER_DATA_DIR}/security/role/geonode REST role service/config.xml.orig"
-    # run the setting script for geonode REST role service
-    /usr/local/tomcat/tmp/set_geoserver_auth.sh "${GEOSERVER_DATA_DIR}/security/role/geonode REST role service/config.xml" "${GEOSERVER_DATA_DIR}/security/role/geonode REST role service/" ${TAGNAME} >> /usr/local/tomcat/tmp/set_geoserver_auth.log
-
-    # set oauth2 filter tagname
-    TAGNAME=( "accessTokenUri" "userAuthorizationUri" "redirectUri" "checkTokenEndpointUrl" "logoutUri" )
-
-    # backup geonode-oauth2 config.xml
-    cp ${GEOSERVER_DATA_DIR}/security/filter/geonode-oauth2/config.xml ${GEOSERVER_DATA_DIR}/security/filter/geonode-oauth2/config.xml.orig
-    # run the setting script for geonode-oauth2
-    /usr/local/tomcat/tmp/set_geoserver_auth.sh ${GEOSERVER_DATA_DIR}/security/filter/geonode-oauth2/config.xml ${GEOSERVER_DATA_DIR}/security/filter/geonode-oauth2/ "${TAGNAME[@]}" >> /usr/local/tomcat/tmp/set_geoserver_auth.log
-
-    # set global tagname
-    TAGNAME=( "proxyBaseUrl" )
-
-    # backup global.xml
-    cp ${GEOSERVER_DATA_DIR}/global.xml ${GEOSERVER_DATA_DIR}/global.xml.orig
-    # run the setting script for global configuration
-    /usr/local/tomcat/tmp/set_geoserver_auth.sh ${GEOSERVER_DATA_DIR}/global.xml ${GEOSERVER_DATA_DIR}/ ${TAGNAME} >> /usr/local/tomcat/tmp/set_geoserver_auth.log
+    echo "DOCKER_HOST_IP is empty so I'll run the python utility" >> /usr/local/tomcat/tmp/set_geoserver_auth.log
+    DOCKER_HOST_IP=`python /usr/local/tomcat/tmp/get_dockerhost_ip.py`
+    echo "The calculated value is now DOCKER_HOST_IP='$DOCKER_HOST_IP'" >> /usr/local/tomcat/tmp/set_geoserver_auth.log
 
 else
-    echo "DOCKER_HOST_IP is empty so I'll leave the original files" >> /usr/local/tomcat/tmp/set_geoserver_auth.log
+    echo "DOCKER_HOST_IP is filled so I'll leave the found value '$DOCKER_HOST_IP'" >> /usr/local/tomcat/tmp/set_geoserver_auth.log
 
 fi
+# set basic tagname
+TAGNAME=( "baseUrl" )
+
+# backup geonodeAuthProvider config.xml
+cp ${GEOSERVER_DATA_DIR}/security/auth/geonodeAuthProvider/config.xml ${GEOSERVER_DATA_DIR}/security/auth/geonodeAuthProvider/config.xml.orig
+# run the setting script for geonodeAuthProvider
+/usr/local/tomcat/tmp/set_geoserver_auth.sh ${GEOSERVER_DATA_DIR}/security/auth/geonodeAuthProvider/config.xml ${GEOSERVER_DATA_DIR}/security/auth/geonodeAuthProvider/ ${TAGNAME} >> /usr/local/tomcat/tmp/set_geoserver_auth.log
+
+# backup geonode REST role service config.xml
+cp "${GEOSERVER_DATA_DIR}/security/role/geonode REST role service/config.xml" "${GEOSERVER_DATA_DIR}/security/role/geonode REST role service/config.xml.orig"
+# run the setting script for geonode REST role service
+/usr/local/tomcat/tmp/set_geoserver_auth.sh "${GEOSERVER_DATA_DIR}/security/role/geonode REST role service/config.xml" "${GEOSERVER_DATA_DIR}/security/role/geonode REST role service/" ${TAGNAME} >> /usr/local/tomcat/tmp/set_geoserver_auth.log
+
+# set oauth2 filter tagname
+TAGNAME=( "accessTokenUri" "userAuthorizationUri" "redirectUri" "checkTokenEndpointUrl" "logoutUri" )
+
+# backup geonode-oauth2 config.xml
+cp ${GEOSERVER_DATA_DIR}/security/filter/geonode-oauth2/config.xml ${GEOSERVER_DATA_DIR}/security/filter/geonode-oauth2/config.xml.orig
+# run the setting script for geonode-oauth2
+/usr/local/tomcat/tmp/set_geoserver_auth.sh ${GEOSERVER_DATA_DIR}/security/filter/geonode-oauth2/config.xml ${GEOSERVER_DATA_DIR}/security/filter/geonode-oauth2/ "${TAGNAME[@]}" >> /usr/local/tomcat/tmp/set_geoserver_auth.log
+
+# set global tagname
+TAGNAME=( "proxyBaseUrl" )
+
+# backup global.xml
+cp ${GEOSERVER_DATA_DIR}/global.xml ${GEOSERVER_DATA_DIR}/global.xml.orig
+# run the setting script for global configuration
+/usr/local/tomcat/tmp/set_geoserver_auth.sh ${GEOSERVER_DATA_DIR}/global.xml ${GEOSERVER_DATA_DIR}/ ${TAGNAME} >> /usr/local/tomcat/tmp/set_geoserver_auth.log
 
 # start tomcat
 exec catalina.sh run
