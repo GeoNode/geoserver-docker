52c2f4568afd559d436cf55fc206d7719f6513e9
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-12-28 16:50:59 +0100

    Improve nginx ip address discovering

--- get_nginxhost_ip.py
+++ get_nginxhost_ip.py
@@ -8,24 +8,32 @@ client = docker.from_env()
 # print client.info()
 
 for network in client.networks.list():
-    if cmp(network.containers, client.containers.list()) is 1:
+    # if cmp(network.containers, client.containers.list()) is 1:
+    if 'geonode' in network.name:
         geonode_network = network.name
+    else:
+        geonode_network = 'geonode_default'
 
-if geonode_network:
+try:
     containers = {
         c.attrs['Config']['Image']: c.attrs['NetworkSettings']['\
 Networks'][geonode_network]['\
 IPAddress'] for c in client.containers.list() if c.status in 'running'
     }
+    for item in containers.items():
+        if "geonode/nginx" in item[0]:
+            ipaddr = item[1]
+
+    try:
+        os.environ["NGINX_BASE_URL"] = "http://" + ipaddr + ":" + "80"
+        print "http://{}:80".format(ipaddr)
+    except NameError as er:
+        raise Exception("NGINX container is not running maybe exited! Running\
+containers are:{0}".format(containers))
+except KeyError as e:
+    raise Exception("There has been a problem with the docker\
+network which has raised the following exception: {0}".format(e))
 else:
+    print "There is no ip address for Nginx within the GeoNode network!"
+finally:
     pass
-
-for item in containers.items():
-    if "geonode/nginx" in item[0]:
-        ipaddr = item[1]
-
-if ipaddr:
-    os.environ["NGINX_BASE_URL"] = "http://" + ipaddr + ":" + "80"
-    print "http://{}:80".format(ipaddr)
-else:
-    print "NGINX container is not running maybe exited!"
