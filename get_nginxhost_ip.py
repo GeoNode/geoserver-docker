051079c54834af023d55679e79f05894ce72764f
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-12-28 23:03:41 +0100

    Set default nginx base url if no running container

--- get_nginxhost_ip.py
+++ get_nginxhost_ip.py
@@ -8,7 +8,6 @@ client = docker.from_env()
 # print client.info()
 
 for network in client.networks.list():
-    # if cmp(network.containers, client.containers.list()) is 1:
     if 'geonode' in network.name:
         geonode_network = network.name
     else:
@@ -30,9 +29,9 @@ IPAddress'] for c in client.containers.list() if c.status in 'running'
     except NameError as er:
         raise Exception("NGINX container is not running maybe exited! Running\
 containers are:{0}".format(containers))
-except KeyError as e:
+except KeyError as ke:
     raise Exception("There has been a problem with the docker\
-network which has raised the following exception: {0}".format(e))
+network which has raised the following exception: {0}".format(ke))
 else:
     # nginx_base_url = None
     pass
@@ -40,4 +39,4 @@ finally:
     try:
         print nginx_base_url
     except NameError as ne:
-        print "There is no ip address for Nginx within the GeoNode network!"
+        print "http://geonode:80"
