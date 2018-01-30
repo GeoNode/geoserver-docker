d8b0a8e965deb6786113a06226bf7c0c4cf336b7
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-01-30 17:31:18 +0100

    Add control for multiple docker host ip addresses

--- get_dockerhost_ip.py
+++ get_dockerhost_ip.py
@@ -3,6 +3,21 @@
 import docker
 
 BOOTSTRAP_IMAGE_CHEIP = 'codenvy/che-ip:nightly'
+print _docker_host_ip()
 
-client = docker.from_env()
-print client.containers.run(BOOTSTRAP_IMAGE_CHEIP, network_mode='host')
+
+def _docker_host_ip():
+    client = docker.from_env()
+    ip_list = client.containers.run(BOOTSTRAP_IMAGE_CHEIP,
+                                    network_mode='host'
+                                    ).split("\n")
+    if len(ip_list) > 1:
+        print("Docker daemon is running on more than one \
+address {0}".format(ip_list))
+        print("Only the first address:{0} will be returned!".format(
+            ip_list[0]
+        ))
+    else:
+        print("Docker daemon is running at the following \
+address {0}".format(ip_list[0]))
+    return ip_list[0]
