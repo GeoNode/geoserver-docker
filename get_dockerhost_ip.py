3bae70390b0daa8315fda7d86a0d37eb7fa7aa86
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-02-12 11:18:29 +0100

    Fix function returning value

--- get_dockerhost_ip.py
+++ get_dockerhost_ip.py
@@ -12,12 +12,12 @@ def _docker_host_ip():
                                     network_mode='host'
                                     ).split("\n")
     if len(ip_list) > 1:
-        print("Docker daemon is running on more than one \
+        logging.info("Docker daemon is running on more than one \
 address {0}".format(ip_list))
-        print("Only the first address:{0} will be returned!".format(
+        logging.info("Only the first address:{0} will be returned!".format(
             ip_list[0]
         ))
     else:
-        print("Docker daemon is running at the following \
+        logging.info("Docker daemon is running at the following \
 address {0}".format(ip_list[0]))
     return ip_list[0]
